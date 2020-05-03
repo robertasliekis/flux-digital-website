23fdaba559bfd90ef1a610c59ea268744dcb4736
robertasliekis <60491171+robertasliekis@users.noreply.github.com>
2020-05-03 15:00:46 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 if (window.innerWidth <= 1024) {
-  $(".overlay-menu-wrapper").height(window.innerHeight);
+  $(".overlay-menu-wrapper").height(window.innerHeight - 56);
 }
 
 window.addEventListener("resize", () => {
-  $(".overlay-menu-wrapper").height(window.innerHeight);
+  $(".overlay-menu-wrapper").height(window.innerHeight - 56);
 });
 
 //Burger menu control start
